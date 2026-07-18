e3d6febfb376c3be6f45c4b5de70630149d857a2
wongkino <wongkino@users.noreply.github.com>
2026-07-19 00:05:10 +0800

    Add local installer, PM2, splashscreen, and start scripts.

---
+++ installers/mm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $(dirname "$0")/..
+
+if [ $(ps -ef | grep -v grep | grep -i -e xway -e labwc | wc -l) -ne 0 ]; then 
+   # if WAYLAND_DISPLAYis set, use it, else set to -0
+   export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:=wayland-0}
+   npm run start:wayland
+else
+   DISPLAY=:0 npm run start:x11
+fi 
+
+#DISPLAY=:0 npm start  
